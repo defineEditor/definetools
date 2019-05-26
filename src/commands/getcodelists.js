8d95231f4e61e1a7636dc6305c5a9f56399030bd
Sergei Krivtcov <1000eyed@gmail.com>
2019-05-26 19:49:35 +0200

    Added extended and format options;

--- src/commands/getcodelists.js
+++ src/commands/getcodelists.js
@@ -1,4 +1,5 @@
 const { Command, flags } = require('@oclif/command');
+const { getDescription } = require('../utils/defineStructureUtils.js');
 const parseDefine = require('../parsers/parseDefine.js');
 const readXml = require('../utils/readXml.js');
 const path = require('path');
@@ -15,12 +16,12 @@ class getCodeLists extends Command {
         // Paths
         const currentFolder = process.cwd();
         let inputFile = path.resolve(currentFolder, argv[0]);
-        let outputFile = path.resolve(currentFolder, argv.length > 1 ? argv[1] : 'codelists.csv');
+        let outputFile = path.resolve(currentFolder, argv.length > 1 ? argv[1] : ('codelists.' + flags.format));
 
         // Read-in and parse XML
         let xmlData = await readXml(inputFile);
         let odm = parseDefine(xmlData);
-        let attributes = fetchData(odm, flags);
+        let data = fetchData(odm, flags);
 
         // Handle filter
         if (flags.filter) {
@@ -35,12 +36,12 @@ class getCodeLists extends Command {
                 this.log(`Invalid filter value. ${error}`);
             }
 
-            attributes = attributes.filter(item => regexFilter.test(item.name));
+            data = data.filter(item => regexFilter.test(item.name));
         }
 
         // put log message
         if (flags.verbose) {
-            let message = `Found ${attributes.length} codelists.` + (attributes.length === 0
+            let message = `Found ${data.length} codelists.` + (data.length === 0
                 ? (flags.stdout ? `` : ` ${outputFile} was not updated.`)
                 : (flags.stdout ? ` Here they are:` : ` The codelists were written to ${outputFile}`)
             );
@@ -48,17 +49,25 @@ class getCodeLists extends Command {
         }
 
         // report
-        if (attributes.length > 0) {
+        if (data.length > 0) {
             if (flags.stdout) {
-                this.log(json2csv.parse(attributes));
+                if (flags.format === 'csv') {
+                    this.log(json2csv.parse(data));
+                } else {
+                    this.log(JSON.stringify(data, null, 2));
+                }
             } else {
-                await writeFile(outputFile, json2csv.parse(attributes));
+                if (flags.format === 'csv') {
+                    await writeFile(outputFile, json2csv.parse(data));
+                } else {
+                    await writeFile(outputFile, JSON.stringify(data, null, 2));
+                }
             }
         }
     }
 }
 
-getCodeLists.description = `Extract variable attributes from a Define-XML file.
+getCodeLists.description = `Extract variable data from a Define-XML file.
 A file created using Define-XML 2.0 standard is expected as an input.
 If the output file is not specified, attrs.csv will be used.
 `;
@@ -70,9 +79,10 @@ getCodeLists.args = [
 
 getCodeLists.flags = {
     verbose: flags.boolean({ char: 'v', description: 'Show additional information during the execution' }),
-    extended: flags.boolean({ char: 'e', description: 'Show extended codelist attributes' }),
-    filter: flags.string({ description: "Regex used to specify datasets to output. Use --filter='^(ae|cm|lb)$' to select AE, CM, and LB datasets." }),
+    extended: flags.boolean({ char: 'e', description: 'Show extended codelist data' }),
+    filter: flags.string({ description: "Regex used to filter the output. Use --filter='^(arm|lbtest|aeout)$' to select ARM, LBTEST, and AEOUT codelists." }),
     stdout: flags.boolean({ description: 'Print results to STDOUT' }),
+    format: flags.string({ char: 'f', description: 'Output format', options: ['csv', 'json'], default: 'csv' }),
 };
 
 function fetchData (odm, flags) {
@@ -83,6 +93,7 @@ function fetchData (odm, flags) {
             if (flags.extended) {
                 return {
                     name: codeList.name,
+                    label: getDescription(codeList),
                     dataType: codeList.dataType,
                     codeListType: codeList.codeListType,
                 };
